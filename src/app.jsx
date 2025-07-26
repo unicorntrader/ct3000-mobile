5fcda2cfc76304c8efeaffefd447369b0e5399b2
unicorntrader <antonis@protopapas.net>
2025-07-26 13:33:00 +0300

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TradingJournal from './TradingJournal';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TradingJournal />
+    </div>
+  );
+}
+
+export default App;
