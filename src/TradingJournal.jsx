3106f06f0b8cceeda49883187e8a301b4b23e71f
unicorntrader <antonis@protopapas.net>
2025-07-26 15:35:08 +0300

    Update TradingJournal.jsx

--- src/TradingJournal.jsx
+++ src/TradingJournal.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import { useTradingState } from './hooks/useTradingState';
-import { Dashboard, PlanTrader, Journal } from './components/views';
+import { Dashboard } from './components/views';
 import { DesktopAdapter } from './components/adapters';
 
 function TradingJournal() {
   const tradingState = useTradingState();
 
-  const renderActiveModule = () => {
-    switch (tradingState.activeModule) {
-      case 'dashboard':
-        return <Dashboard {...tradingState} />;
-      case 'plan-trader':
-        return <PlanTrader {...tradingState} />;
-      case 'notebook':
-        return <Journal {...tradingState} />;
-      case 'smart-journal':
-        return <div className="p-6 text-center text-gray-500">Smart Journal - Coming Soon</div>;
-      case 'daily-view':
-        return <div className="p-6 text-center text-gray-500">Daily View - Coming Soon</div>;
-      case 'performance':
-        return <div className="p-6 text-center text-gray-500">Performance - Coming Soon</div>;
-      default:
-        return <Dashboard {...tradingState} />;
-    }
-  };
-
   return (
     <DesktopAdapter {...tradingState}>
-      {() => renderActiveModule()}
+      {(props) => <Dashboard {...props} />}
     </DesktopAdapter>
   );
 }
